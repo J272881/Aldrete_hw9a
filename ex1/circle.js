e80aa413e3e0bb92242f664d470749bb07882068
Jon Aldrete <jon.aldrete9@gmail.com>
2022-04-21 01:18:57 -0700

    Aldrete_hw9a(1.0)

--- ex1/circle.js
+++ ex1/circle.js
@@ -1,2 +1,7 @@
-console.log('Hello World');
-console.log('hi')
+const PI = Math.PI;
+
+// Return the circumference of a circle
+module.exports.circumference = radius => 2 * PI * radius;
+
+// Return the area of a circle
+module.exports.area = radius => PI * radius ** 2;
